39bab19f937ffef4c7803c78cd3bbf9fce437d5d
sashachen1002 <118510551+sashachen1002@users.noreply.github.com>
2023-06-01 11:48:35 +0800

    Add query into WindowDemo.java

--- src/WindowDemo.java
+++ src/WindowDemo.java
@@ -1,83 +1,94 @@
-import java.awt.EventQueue;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
-import java.awt.BorderLayout;
 import javax.swing.JTextArea;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
-import javax.swing.JPanel;
-import javax.swing.SwingConstants;
 import javax.swing.filechooser.FileNameExtensionFilter;
-import javax.swing.JRadioButton;
+
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 import java.awt.Font;
 
 public class WindowDemo extends JFrame{
 	
-    JButton button;
-    JLabel label;
+	private JButton browseButton;
+    private JLabel label;
+    private JTextArea postTextArea;
+    private JComboBox typeComboBox;
+    private JTextField text_name;
     private JTextField text_location;
     private JTextField text_startTime;
-    private JTextField item_num;
-    private JTextField textField;
-    private JTextField textField_1;
+    private JTextField text_price;
+    private JTextField text_endTime;
+    private JTextField text_amount;
+    private File file_path;
+	private byte[] imageBytes;
+    
+	private SQLQuery sqlQuery = new SQLQuery();
     
     public WindowDemo(){
-	    button = new JButton("Browse");
-	    button.setFont(new Font("微軟正黑體", Font.PLAIN, 12));
-	    button.setBounds(180,241,100,40);
-	    label = new JLabel();
-	    label.setBounds(323,237,359,280);
+	    browseButton = new JButton("Browse");
+	    browseButton.setFont(new Font("微軟正黑體", Font.PLAIN, 12));
+	    browseButton.setBounds(180,241,100,40);
 	    getContentPane().setLayout(null);
-	    getContentPane().add(button);
-	    getContentPane().add(label);
+	    getContentPane().add(browseButton);
 	    
-	    button.addActionListener(new ActionListener() {
-	
-	        public void actionPerformed(ActionEvent e) {
-	        
-	          JFileChooser file = new JFileChooser();
-	          file.setCurrentDirectory(new File(System.getProperty("user.home")));
-	          //filter the files
-	          FileNameExtensionFilter filter = new FileNameExtensionFilter("*.Images", "jpg","gif","png");
-	          file.addChoosableFileFilter(filter);
-	          int result = file.showSaveDialog(null);
-	           //if the user click on save in Jfilechooser
-	          if(result == JFileChooser.APPROVE_OPTION){
-	              File selectedFile = file.getSelectedFile();
-	              String path = selectedFile.getAbsolutePath();
-	              label.setIcon(ResizeImage(path));
-	          }
+	    browseButton.addActionListener(new ActionListener() {
+	    	public void actionPerformed(ActionEvent e) {
+				if(e.getSource()==browseButton) {
+					JFileChooser file_upload = new JFileChooser();
+					FileNameExtensionFilter filter = new FileNameExtensionFilter("Image Files", "jpg", "jpeg", "png", "gif");
+				    file_upload.setFileFilter(filter);
 
-	          else if(result == JFileChooser.CANCEL_OPTION){
-	              System.out.println("No File Select");
-	          }
-	        }
+					int res=file_upload.showSaveDialog(null);
+
+					if(res==JFileChooser.APPROVE_OPTION) {
+						file_path=new File(file_upload.getSelectedFile().getAbsolutePath());
+					    try {
+					    	imageBytes = Files.readAllBytes(file_path.toPath());
+							ImageIcon icon = new ImageIcon(imageBytes);
+							Image scaledImage = icon.getImage().getScaledInstance(359,280, Image.SCALE_SMOOTH);
+							label = new JLabel(new ImageIcon(scaledImage));
+							label.setBounds(323,237,359,280);
+							getContentPane().add(label);
+							getContentPane().revalidate();
+							getContentPane().repaint();
+							
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+
+					}
+				}
+			}	
 	    });
 
-	    JTextArea postTextArea = new JTextArea();
+	    postTextArea = new JTextArea();
 	    postTextArea.setFont(new Font("微軟正黑體", Font.PLAIN, 13));
 	    postTextArea.setText("請輸入貼文內容");
-	    postTextArea.setBounds(29, 51, 670, 176);
+	    postTextArea.setBounds(29, 97, 670, 130);
 	    getContentPane().add(postTextArea);
 	    
-	    JLabel text_1 = new JLabel("插入貼文照片");
-	    text_1.setFont(new Font("微軟正黑體", Font.PLAIN, 12));
-	    text_1.setBounds(29, 241, 108, 40);
-	    getContentPane().add(text_1);
+	    JLabel imageLabel = new JLabel("插入貼文照片");
+	    imageLabel.setFont(new Font("微軟正黑體", Font.PLAIN, 12));
+	    imageLabel.setBounds(29, 241, 108, 40);
+	    getContentPane().add(imageLabel);
 	    
-	    JLabel text_2 = new JLabel("領取地點");
-	    text_2.setFont(new Font("微軟正黑體", Font.PLAIN, 12));
-	    text_2.setBounds(29, 291, 108, 40);
-	    getContentPane().add(text_2);
+	    JLabel locationLabel = new JLabel("領取地點");
+	    locationLabel.setFont(new Font("微軟正黑體", Font.PLAIN, 12));
+	    locationLabel.setBounds(29, 291, 108, 40);
+	    getContentPane().add(locationLabel);
 	    
 	    text_location = new JTextField();
 	    text_location.setFont(new Font("微軟正黑體", Font.PLAIN, 12));
@@ -85,21 +96,21 @@ public class WindowDemo extends JFrame{
 	    getContentPane().add(text_location);
 	    text_location.setColumns(10);
 	    
-	    JLabel text_3 = new JLabel("食物類型");
-	    text_3.setFont(new Font("微軟正黑體", Font.PLAIN, 12));
-	    text_3.setBounds(29, 341, 108, 40);
-	    getContentPane().add(text_3);
+	    JLabel typeLabel = new JLabel("食物類型");
+	    typeLabel.setFont(new Font("微軟正黑體", Font.PLAIN, 12));
+	    typeLabel.setBounds(29, 341, 108, 40);
+	    getContentPane().add(typeLabel);
 	    
-	    JComboBox comboBox = new JComboBox();
-	    comboBox.setFont(new Font("微軟正黑體", Font.PLAIN, 12));
-	    comboBox.setModel(new DefaultComboBoxModel(new String[] {"便當", "飲料", "零食", "生鮮食材"}));
-	    comboBox.setBounds(180, 350, 100, 23);
-	    getContentPane().add(comboBox);
+	    typeComboBox = new JComboBox();
+	    typeComboBox.setFont(new Font("微軟正黑體", Font.PLAIN, 12));
+	    typeComboBox.setModel(new DefaultComboBoxModel(new String[] {"便當", "飲料", "零食", "生鮮食材"}));
+	    typeComboBox.setBounds(180, 350, 100, 23);
+	    getContentPane().add(typeComboBox);
 	    
-	    JLabel text_5 = new JLabel("開始發放時間 (e.g 13:30)");
-	    text_5.setFont(new Font("微軟正黑體", Font.PLAIN, 12));
-	    text_5.setBounds(29, 407, 141, 40);
-	    getContentPane().add(text_5);
+	    JLabel startTimeLabel = new JLabel("開始發放時間 (e.g 13:30)");
+	    startTimeLabel.setFont(new Font("微軟正黑體", Font.PLAIN, 12));
+	    startTimeLabel.setBounds(29, 407, 141, 40);
+	    getContentPane().add(startTimeLabel);
 	    
 	    text_startTime = new JTextField();
 	    text_startTime.setFont(new Font("微軟正黑體", Font.PLAIN, 12));
@@ -107,52 +118,95 @@ public class WindowDemo extends JFrame{
 	    text_startTime.setBounds(180, 419, 100, 21);
 	    getContentPane().add(text_startTime);
 	    
-	    JLabel text_6 = new JLabel("每份最低售價 (無可填0)");
-	    text_6.setFont(new Font("微軟正黑體", Font.PLAIN, 12));
-	    text_6.setBounds(29, 484, 141, 40);
-	    getContentPane().add(text_6);
+	    JLabel priceLabel = new JLabel("每份最低售價 (無可填0)");
+	    priceLabel.setFont(new Font("微軟正黑體", Font.PLAIN, 12));
+	    priceLabel.setBounds(29, 484, 141, 40);
+	    getContentPane().add(priceLabel);
 	    
-	    item_num = new JTextField();
-	    item_num.setFont(new Font("微軟正黑體", Font.PLAIN, 12));
-	    item_num.setColumns(10);
-	    item_num.setBounds(180, 494, 100, 21);
-	    getContentPane().add(item_num);
-	    
-	    JButton postButton = new JButton("發佈貼文");
-	    postButton.setFont(new Font("微軟正黑體", Font.BOLD, 14));
-	    postButton.setBounds(29, 546, 670, 40);
-	    getContentPane().add(postButton);
+	    text_price = new JTextField();
+	    text_price.setFont(new Font("微軟正黑體", Font.PLAIN, 12));
+	    text_price.setColumns(10);
+	    text_price.setBounds(180, 494, 100, 21);
+	    getContentPane().add(text_price);
 	    
 	    JLabel username = new JLabel("username");
 	    username.setFont(new Font("微軟正黑體", Font.PLAIN, 14));
 	    username.setBounds(29, 0, 757, 54);
 	    getContentPane().add(username);
 	    
-	    JLabel text_5_1 = new JLabel("領取截止時間 (e.g. 18:00)");
-	    text_5_1.setFont(new Font("微軟正黑體", Font.PLAIN, 12));
-	    text_5_1.setBounds(27, 442, 141, 40);
-	    getContentPane().add(text_5_1);
+	    JLabel endTimeLabel = new JLabel("領取截止時間 (e.g. 18:00)");
+	    endTimeLabel.setFont(new Font("微軟正黑體", Font.PLAIN, 12));
+	    endTimeLabel.setBounds(27, 442, 141, 40);
+	    getContentPane().add(endTimeLabel);
 	    
-	    textField = new JTextField();
-	    textField.setFont(new Font("微軟正黑體", Font.PLAIN, 12));
-	    textField.setColumns(10);
-	    textField.setBounds(180, 450, 100, 21);
-	    getContentPane().add(textField);
+	    text_endTime = new JTextField();
+	    text_endTime.setFont(new Font("微軟正黑體", Font.PLAIN, 12));
+	    text_endTime.setColumns(10);
+	    text_endTime.setBounds(180, 450, 100, 21);
+	    getContentPane().add(text_endTime);
 	    
-	    JLabel text_6_1 = new JLabel("發放數量");
-	    text_6_1.setFont(new Font("微軟正黑體", Font.PLAIN, 12));
-	    text_6_1.setBounds(29, 372, 141, 40);
-	    getContentPane().add(text_6_1);
+	    JLabel amountLabel = new JLabel("發放數量");
+	    amountLabel.setFont(new Font("微軟正黑體", Font.PLAIN, 12));
+	    amountLabel.setBounds(29, 372, 141, 40);
+	    getContentPane().add(amountLabel);
 	    
-	    textField_1 = new JTextField();
-	    textField_1.setFont(new Font("微軟正黑體", Font.PLAIN, 12));
-	    textField_1.setColumns(10);
-	    textField_1.setBounds(180, 384, 100, 21);
-	    getContentPane().add(textField_1);
+	    text_amount = new JTextField();
+	    text_amount.setFont(new Font("微軟正黑體", Font.PLAIN, 12));
+	    text_amount.setColumns(10);
+	    text_amount.setBounds(180, 384, 100, 21);
+	    getContentPane().add(text_amount);
 	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    setLocationRelativeTo(null);
 	    setSize(800,800);
 	    setVisible(true);
+	    
+	    JButton postButton = new JButton("發佈貼文");
+	    postButton.setFont(new Font("微軟正黑體", Font.BOLD, 14));
+	    postButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				String name = text_name.getText();
+				String postContent = postTextArea.getText();
+				byte[] image = imageBytes;
+				String location = text_location.getText();
+				String type = (String)typeComboBox.getSelectedItem();
+				int amount = Integer.parseInt(text_amount.getText());
+				String startTime = text_startTime.getText();
+				String endTime = text_endTime.getText();
+				int price = Integer.parseInt(text_price.getText());
+				
+				ProcessData uploadProduct = new ProcessData(name,postContent,image,location,type,amount,startTime,endTime,price);
+				sqlQuery.uploadProduct(uploadProduct);
+				
+				//檢查上架是否成功
+                boolean success = ((SQLQuery) sqlQuery).uploadProduct(uploadProduct);
+                if (!success) {
+                	JOptionPane.showMessageDialog(null, "Please complete the text.", "Upload Failed", JOptionPane.ERROR_MESSAGE);
+                }else {
+                	//上架成功後關閉視窗 即可回到首頁
+                	JOptionPane.showMessageDialog(null, "Uploaded Successfully！", "Success", JOptionPane.INFORMATION_MESSAGE);
+                	WindowDemo.this.dispose();
+    		        // 連結到 HomePage
+    		        HomePage homePage = new HomePage();
+    		        homePage.setVisible(true);
+                }
+			
+			}
+		});
+	    postButton.setBounds(29, 546, 670, 40);
+	    getContentPane().add(postButton);
+	    
+	    JLabel NameLabel = new JLabel("\u98DF\u7269\u540D\u7A31");
+	    NameLabel.setFont(new Font("微軟正黑體", Font.PLAIN, 12));
+	    NameLabel.setBounds(29, 47, 108, 40);
+	    getContentPane().add(NameLabel);
+	    
+	    text_name = new JTextField();
+	    text_name.setFont(new Font("微軟正黑體", Font.PLAIN, 12));
+	    text_name.setColumns(10);
+	    text_name.setBounds(180, 57, 100, 21);
+	    getContentPane().add(text_name);
+	    
 	    }
      
 	     // Method to resize imageIcon with the same size of a Jlabel
