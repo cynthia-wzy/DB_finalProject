d5a0e0278c920c1aec46388c3466e359ced5fe1b
Cynthia <77578840+cynthia-wzy@users.noreply.github.com>
2023-06-14 19:12:29 +0800

    Update PostInfo.java

--- src/PostInfo.java
+++ src/PostInfo.java
@@ -1,13 +1,15 @@
 public class PostInfo {
 	private byte[] image;
+	private int postID;
     private String foodName;
     private String foodLocation;
     private int foodAmount;
     private String pickupDDL;
     private int minPrice;
 
-    public PostInfo(byte[] image, String foodName, String foodLocation, int foodAmount, String pickupDDL, int minPrice) {
+    public PostInfo(byte[] image, int postID, String foodName, String foodLocation, int foodAmount, String pickupDDL, int minPrice) {
         this.image = image;
+        this.postID = postID;
     	this.foodName = foodName;
         this.foodLocation = foodLocation;
         this.foodAmount = foodAmount;
@@ -15,52 +17,34 @@ public class PostInfo {
         this.minPrice = minPrice;
     }
 
-    // getter & setter
+    
+
+	// getters
     public byte[] getImage() {
 		return image;
 	}
-
-	public void setImage(byte[] image) {
-		this.image = image;
+	
+	public int getPostID() {
+		return postID;
 	}
 	
 	public String getFoodName() {
         return foodName;
     }
 
-	public void setFoodName(String foodName) {
-        this.foodName = foodName;
-    }
-
     public String getFoodLocation() {
         return foodLocation;
-    }
-
-    public void setFoodLocation(String foodLocation) {
-        this.foodLocation = foodLocation;
     }
 
     public int getFoodAmount() {
         return foodAmount;
     }
 
-    public void setFoodAmount(int foodAmount) {
-        this.foodAmount = foodAmount;
-    }
-
     public String getPickupDDL() {
         return pickupDDL;
     }
 
-    public void setPickupDDL(String pickupDDL) {
-        this.pickupDDL = pickupDDL;
-    }
-
     public int getMinPrice() {
         return minPrice;
-    }
-
-    public void setMinPrice(int minPrice) {
-        this.minPrice = minPrice;
     }
 }
